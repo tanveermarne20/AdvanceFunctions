7a5d6ad708beee059cfb0d72f46023a7ef24f466
tanveermarne20 <>
2024-02-07 16:55:58 +0530

    initial

---
+++ View.sql
@@ -0,0 +1,17 @@
+--====================================================================================================
+-- 01.What is view?
+--Ans.View is a database object which is created over an SQL query
+--      View just represent data that is just return by sql querry
+--      View does not stores data,but every time you execute querry
+--      it just execute the sql querry
+--      View is like a viirtual table     
+--      The difference between Table and View is 
+--        The table can store data but view does not
+
+SELECT *FROM TB_CUSTOMER_DATA;
+SELECT *FROM TB_PRODUCT_INFO;
+SELECT *FROM TB_ORDER_DETAILS;
+
+
+
+
